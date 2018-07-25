a387c1af423d83bde425cb5787c2791d9f1c018c
Patrick White <patrickwhite.256@gmail.com>
2018-07-25 11:25:44 -0700

    update mac things

--- mac-utils.sh
+++ mac-utils.sh
@@ -38,31 +38,26 @@ cyan " - pick"
 if [ -s ~/bin/pick ]; then
     cyan "  - Found ~/bin/pick already, skipping"
 else
-    dep_check git autoconf automake cmake g++
-    if [ $? -ne 0 ]; then
-        red "  - Dependencies not met, skipping install"
-    else
-        (
-            set -e
-            wget https://github.com/thoughtbot/pick/releases/download/v$PICK_VER/pick-$PICK_VER.tar.gz
-            tar xzvf pick-$PICK_VER.tar.gz
-            mkdir -p ~/lib/pick
-            cd pick-$PICK_VER
-            ./configure --prefix=$HOME/lib/pick
-            make
-            make install
+    (
+        set -e
+        wget https://github.com/thoughtbot/pick/releases/download/v$PICK_VER/pick-$PICK_VER.tar.gz
+        tar xzvf pick-$PICK_VER.tar.gz
+        mkdir -p ~/lib/pick
+        cd pick-$PICK_VER
+        ./configure --prefix=$HOME/lib/pick
+        make
+        make install
 
-            cd $root_dir
-            rm pick-$PICK_VER.tar.gz
-            rm -r pick-$PICK_VER
-            ln -s $HOME/lib/pick/bin/pick ~/bin/pick
-        ) > $root_dir/logs/pick 2>&1
-        # http://unix.stackexchange.com/questions/65532/why-does-set-e-not-work-inside
-        if [ "$?" -ne 0 ]; then
-            red " - Something went wrong. Check logs/pick."
-        else
-            green "  - Success!"
-        fi
+        cd $root_dir
+        rm pick-$PICK_VER.tar.gz
+        rm -r pick-$PICK_VER
+        ln -s $HOME/lib/pick/bin/pick ~/bin/pick
+    ) > $root_dir/logs/pick 2>&1
+    # http://unix.stackexchange.com/questions/65532/why-does-set-e-not-work-inside
+    if [ "$?" -ne 0 ]; then
+        red " - Something went wrong. Check logs/pick."
+    else
+        green "  - Success!"
     fi
 fi
 
@@ -86,26 +81,8 @@ else
 fi
 
 cyan " - diff-so-fancy"
-if [ -s ~/bin/diff-so-fancy ]; then
-    cyan "  - Found ~/bin/diff-so-fancy already, skipping"
+if [ -n "$(brew ls --versions diff-so-fancy)" ]; then
+    cyan "   - diff-so-fancy already installed, skipping"
 else
-    dep_check git
-    if [ $? -ne 0 ]; then
-        red "  - Dependencies not met, skipping install"
-    else
-        (
-            set -e
-            git clone https://github.com/so-fancy/diff-so-fancy.git
-            cd diff-so-fancy
-            cp diff-so-fancy libexec/diff-so-fancy.pl third_party/diff-highlight/diff-highlight ~/bin
-            chmod +x ~/bin/diff-highlight
-            cd ..
-            rm -rf diff-so-fancy
-        ) >$root_dir/logs/diff-so-fancy 2>&1
-        if [ "$?" -ne 0 ]; then
-            red " - Something went wrong. Check logs/diff-so-fancy"
-        else
-            green "  - Success!"
-        fi
-    fi
+    brew install diff-so-fancy
 fi
